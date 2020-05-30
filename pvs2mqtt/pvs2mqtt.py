c4f8fc2554e5010cf6e8c348a99975cfbc87cd92
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2020-05-30 20:54:25 +0000

    added mqtt publishing and script

--- pvs2mqtt/pvs2mqtt.py
+++ pvs2mqtt/pvs2mqtt.py
@@ -1,9 +1,12 @@
 import urllib.request, json
-import paho.mqtt.client as mqtt
+import paho.mqtt.publish as mqtt
 
-polling_frequency = '1'         # in minutes
-#mqqt_url = ''
+# mqtt config
+mqtt_host = 'localhost'
+mqtt_port = 1883
+mqtt_topic = 'pvs2mqtt'
 
+# pvs config
 method = 'http'
 host = 'sunpowerconsole.com'
 comm_status_command = 'Get_Comm'
@@ -15,7 +18,20 @@ def get_device_info():
     with urllib.request.urlopen(device_url) as response:
         return json.loads(response.read())
  
+def export_pvs_device_status():
+    info = get_device_info()
+    messages = []
+    for device in info['devices']:      # categorize devices by type / serial number
+        device_type = device['DEVICE_TYPE']
+        serial = device['SERIAL']
+        for key in device.keys():       # update topic for each field in device status
+            messages.append({'topic': mqtt_topic+'/'+device_type+'/'+serial+'/'+key,
+                             'payload': device[key],
+                             'qos': 0,
+                             'retain': True})
+    # publish messages
+    mqtt.multiple(messages, hostname=mqtt_host, port=mqtt_port)
 
 
-
-#print(get_device_info())
+if __name__ == "__main__":              # update mqtt with pvs values
+    export_pvs_device_status()
